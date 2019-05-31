6ad768d90fee9c1ab8d72e44c3ceb731336946a3
Rudolf Cardinal <rudolf@pobox.com>
2019-05-31 14:39:43 +0100

    Age functions

--- datetimefunc.R
+++ datetimefunc.R
@@ -13,7 +13,7 @@ datetimefunc = new.env()
 # Calculate age
 #==============================================================================
 
-datetimefunc$calendarAge <- function(dob, now) {
+datetimefunc$calendar_age_lubridate_1 <- function(dob, now) {
     # Works with POSIXct or Date values
     # http://stackoverflow.com/questions/32312925/time-difference-in-years-with-lubridate
     interval_period <- lubridate::interval(dob, now)
@@ -28,6 +28,34 @@ datetimefunc$calendarAge <- function(dob, now) {
     #     %/% lubridate::days(1)
     # )
     return(full_years)
+}
+
+datetimefunc$calendarAge <- calendar_age_lubridate_1
+
+
+calendar_age <- function(dob, now)
+{
+    # THIS IS BETTER.
+    # https://stackoverflow.com/questions/31126726
+    # https://stackoverflow.com/questions/3611314/calculate-ages-in-r
+    from_lt <- as.POSIXlt(dob)
+    to_lt <- as.POSIXlt(now)
+
+    age <- to_lt$year - from_lt$year
+
+    return(ifelse(
+        to_lt$mon < from_lt$mon |
+            (to_lt$mon == from_lt$mon & to_lt$mday < from_lt$mday),
+        age - 1,
+        age
+    ))
+}
+
+
+age_float_years <- function(dob, now, days_per_year = 365.25)
+{
+    days <- as.numeric(difftime(now, dob, units = "days"))
+    return(days / days_per_year)
 }
 
 
