d51aabef64f22fdebe3f87cea766b23c27fcddc0
Rudolf Cardinal <rudolf@pobox.com>
2020-06-29 16:31:48 +0100

    Update installation code for Ubuntu 20.04

--- INSTALL_MANY_MODULES.R
+++ INSTALL_MANY_MODULES.R
@@ -1,6 +1,12 @@
+#!/usr/bin/env Rscript
+#
 # source("http://egret.psychol.cam.ac.uk/rlib/INSTALL_MANY_MODULES.R")
-
-# Also, in Ubuntu: need modules listed in R_EXTRAS variable at http://egret.psychol.cam.ac.uk/techniques/scripts/rnc_ubuntu_setup
+#
+# Also, in Ubuntu: need modules installed by
+#       install_r_ubuntu.sh
+#
+# A long time ago, it was those listed in the R_EXTRAS variable at
+#       http://egret.psychol.cam.ac.uk/techniques/scripts/rnc_ubuntu_setup
 
 PREFERRED_CRAN_REPOSITORY = c(CRAN="http://cran.ma.imperial.ac.uk")
 
@@ -42,98 +48,93 @@ install_github_if_absent <- function(libname, github_name)
 
 
 DESIRED_LIBRARIES <- c(
-    "arm",
-    "afex",
-    "arrayhelpers",
-    # "base64",
-    # "BMS",
-    "bridgesampling",
-    "broom",
-    "Cairo",
-    "car",
-    "coda",
-    "codetools",
-    "data.table",
-    "devtools",
-    "diagram",
-    "DiagrammeR",
-    "doParallel",
-    "doSNOW",
-    # WHEN RCPP VERSION CATCHES UP # "dplyr",  # Oct 2014
-    # "extrafont", # use Cairo instead
-    "ez",
-    "foreign",
-    "gdata",
-    "ggplot2",
-    "gplots",
-    "grid",
-    "gridExtra",
-    "gtools",
-    "Hmisc",
-    "inline", # for RStan
-    "Kmisc",  # including cat.cb, to write to the clipboard
-    "languageR", # removed from CRAN in 2013; then back 2013-12-12 (needs R 3.0.2) see http://cran.r-project.org/web/packages/languageR/index.html
-    "lattice",
-    "lme4",
+    "arm",  # analysis using regression and multilevel/hierarchical models
+    "afex",  # analysis of factorial experiments
+    "arrayhelpers",  # convenience functions for arrays
+    "bridgesampling",  # bridge sampling for Stan models
+    "broom",  # convert statistical objects to tibbles
+    "Cairo",  # better graphics output (for use with ggplot)
+    "car",  # for car::Anova
+    "coda",  # analysis/diagnostics for Markov chain Monte Carlo (MCMC) models
+    "codetools",  # code analysis tools
+    "conflicted",  # make name conflicts explicit
+    "data.table",  # fast tables
+    "devtools",  # e.g. install R packages from github
+    "diagram",  # visualize simple graphs/networks
+    "DiagrammeR",  # graph/network visualization
+    "doParallel",  # parallel computing
+    "doSNOW",  # use SNOW for parallel computing
+    "ez",  # ezANOVA
+    "foreign",  # read data from SPSS, etc.
+    "gdata",  # miscellaneous data manipulation tools
+    "gplots",  # misc. plotting functions
+    "gridExtra",  # misc. functions for "grid" graphics
+    "gtools",  # programming assistance
+    "Hmisc",  # Harrell Miscellaneous
+    "inline",  # for RStan
+    # NOT AVAILABLE FOR R 3.6.3 # "Kmisc",  # including cat.cb, to write to the clipboard
+    "languageR",  # analysing linguistic data (NLP)
+    "lattice",  # trellis graphics
+    "lme4",  # linear mixed effects modelling
     "LMERConvenienceFunctions",
-    "lmerTest",
-    "lsmeans",
-    "ltm",
-    "lubridate",
-    "maptools",
-    "matrixStats",
-    "MCMCglmm",
-    "nlme",
-    "nortest",
-    "optimParallel",
-    "parallel",
-    "plyr",
-    "plotrix",
-    "popbio",
-    "proto",
-    # "R2jags",
-    # "Rcmdr", # use RStudio instead,
-    "Rcpp", # for RStan
-    "raster",
-    "readODS",
-    "readr",
-    "readxl",
-    "reshape",
-    "reshape2",
-    "rgeos",
-    "rgdal",
-    # "rjags", # also needs some command-line prerequisites
-    "rstan", # if fails: install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
-        # ... see https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
-    "RMySQL",
-    "RODBC",
-    "semver",
-    "shinystan",
-    "snow",
+    "lmerTest",  # p values for lme4::lmer()
+    "lsmeans",  # least-squares means
+    "ltm",  # latent trait models
+    "lubridate",  # better dates/times
+    "maptools",  # for spatial maps
+    "matrixStats",  # high-performance matrix functions
+    "MCMCglmm",  # MCMC generalized linear mixed models
+    "nlme",  # nonlinear mixed effects models
+    "nortest",  # tests for normality
+    "openxlsx",  # manipulate Excel files
+    "optimParallel",  # parallel version of L-BFGS-B method for optim()
+    "parallel",  # parallel computing
+    "patchwork",  # arrange ggplot plots; https://patchwork.data-imaginist.com/
+    # "plyr",  # superseded by dplyr (part of tidyverse)
+    "plotrix",  # plotting functions
+    "popbio",  # matrix population models
+    "proto",  # prototype object-based programming
+    "Rcpp",  # R/C++ integration; for RStan
+    "raster",  # geographic data analysis and modelling
+    "readODS",  # read OpenOffice files
+    "readxl",  # read Excel files
+    "reshape",  # melt, cast
+    "reshape2",  # melt, dcast
+    "rgeos",  # interface to GEOS (Geometry Engine - Open Source)
+    "rgdal",  # geospatial
+    "rstan",  # Stan (https://mc-stan.org/)
+    "RMySQL",  # direct connection to MySQL databases
+    # "RODBC",  # ODBC connector; use DBI or odbc instead; https://www.mainard.co.uk/post/database-connections-in-r/
+    "semver",  # semantic version
+    "sf",  # simple spatial features
+    "shinystan",  # visualize Stan models
+    "snow",  # Simple Network of Workstations (parallel computing)
     "sp",  # spatial, for maps
-    "sqldf",
-    "stringr",
-    "tidyr",
-    "TTR",
-    "visreg",
-    "XLConnect",
-    "xlsx",
-    "xtermStyle",
-    "zoo"
+    "sqldf",  # manipulate data frames using SQL
+    "tidyverse",  # ggplot2, dplyr, tidyr, ... https://www.tidyverse.org/
+    "TTR",  # Technical Trading Rules
+    "visreg",  # visualize regression models
+    "XLConnect",  # read/write/manipulate Excel files
+    "xlsx",  # read/write/manipulate Excel files
+    "xtermStyle",  # terminal text formatting
+    "zoo"  # for time series (ordered observations)
 )
 DESIRED_GITHUB_LIBRARIES <- c(
     bbplot = "bbc/bbplot"  # BBC styles for ggplot2
 )
 
 # First, update packages:
-update.packages(checkBuilt=TRUE, ask=FALSE)
+update.packages(checkBuilt = TRUE, ask = FALSE)
 
 # Now install:
 for (libname in DESIRED_LIBRARIES) {
     install_if_absent(libname)
 }
 for (i in 1:length(DESIRED_GITHUB_LIBRARIES)) {
-    install_github_if_absent(names(DESIRED_GITHUB_LIBRARIES)[i], DESIRED_GITHUB_LIBRARIES[i])
+    install_github_if_absent(
+        names(DESIRED_GITHUB_LIBRARIES)[i],
+        DESIRED_GITHUB_LIBRARIES[i]
+    )
 }
 
 # rstan used to require special procedures, but doesn't any more:
