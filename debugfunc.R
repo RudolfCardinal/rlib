d8c7c297618b69634d6f7ddf52becdd92dc21908
Rudolf Cardinal <rudolf@pobox.com>
2023-11-01 15:44:22 +0000

    Docker tweaks

--- debugfunc.R
+++ debugfunc.R
@@ -1,13 +1,13 @@
 # debugfunc.R
 
-tmp_require_package_namespace <- function(...) {
-    packages <- as.character(match.call(expand.dots = FALSE)[[2]])
-    for (p in packages) if (!requireNamespace(p)) install.packages(p)
-}
-tmp_require_package_namespace(
-    xtermStyle
-)
-rm(tmp_require_package_namespace)
+# tmp_require_package_namespace <- function(...) {
+#     packages <- as.character(match.call(expand.dots = FALSE)[[2]])
+#     for (p in packages) if (!requireNamespace(p)) install.packages(p)
+# }
+# tmp_require_package_namespace(
+#     xtermStyle
+# )
+# rm(tmp_require_package_namespace)
 
 
 # =============================================================================
