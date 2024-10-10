58b71aea43e7f9ab5232cdbfa87f201ca622f8d8
Rudolf Cardinal <rudolf@pobox.com>
2024-10-10 17:42:52 +0100

    error reporting for pulsetables

--- datetimefunc.R
+++ datetimefunc.R
@@ -226,7 +226,7 @@ datetimefunc$mk_pulsetable_dimensionless <- function(
     n_events <- length(event_starts)
     if (n_events != n_event_times && n_durations > 1) {
         cat("--- BAD event_times:\n")
-        print(event_times)
+        dput(event_times)
         stop(paste0(
             "You mustn't supply non-unique event_times if you are providing ",
             "individual values of duration_per_event."
