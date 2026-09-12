2a5525a4c95558dab1f33b4c1f12c972f73767d3
Rudolf Cardinal <rudolf@pobox.com>
2026-09-12 18:03:20 +0100

    Refactor Dockerfile for better R package installation; testing of data.table progress bar

---
+++ docker/install_all_packages.R
@@ -0,0 +1,202 @@
+#!/usr/bin/env Rscript
+# Script called by the Dockerfile to install all R packages. Run as root.
+#
+# =============================================================================
+# Package management system(s)
+# =============================================================================
+# See R_library_notes.R for discussion of package management tools.
+
+cat("- R: Installing pak package manager\n")
+
+install.packages("pak", repos = 'https://r-lib.github.io/p/pak/dev/')
+    # The default version, 0.7.2, frequently crashes.
+    # The development version is (as of 12 Sep 2026) 0.11.1; much better.
+    # See https://github.com/r-lib/pak/issues/655
+
+
+# =============================================================================
+# Already available from the Docker installation to date:
+# =============================================================================
+#
+#   zoo         # For time series (ordered observations).
+#   shinystan   # Visualize Stan models.
+#   broom       # Convert statistical objects to tibbles.
+#   Cairo       # Better graphics output (for use with ggplot).
+#   coda        # Analysis/diagnostics for Markov chain Monte Carlo models.
+#   codetools   # Code analysis tools.
+#   conflicted  # Make name conflicts explicit.
+#   data.table  # Fast tables.
+#   devtools    # E.g. install R packages from Github.
+#   DiagrammeR  # Graph/network visualization.
+#   foreign     # Read data from SPSS, etc.
+#   gridExtra   # Misc. functions for grid graphics.
+#   gtools      # Programming assistance.
+#   inline      # For RStan.
+#   lattice     # Trellis graphics.
+#   lme4        # Linear mixed effects modelling.
+#   lubridate   # Better dates/times. [Tidyverse.]
+#   matrixStats # High-performance matrix functions.
+#   nlme        # Nonlinear mixed effects models.
+#   parallel    # Parallel computing.
+#   RColorBrewer # Colours. Built in?
+#   Rcpp        # R/C++ integration; for RStan.
+#   readxl      # Read Excel files.
+#   reshape     # melt(), cast().
+#   reshape2    # melt(), dcast().
+#   tidyverse   # https://www.tidyverse.org/; e.g. ggplot2, dplyr, tidry, ...
+
+
+# =============================================================================
+# Install more
+# =============================================================================
+
+cat("- R: Installing packages, stage 1\n")
+
+pak::pak(c(
+    "arm",  # Analysis using regression and multilevel/hierarchical models.
+    "afex",  # Analysis of factorial experiments.
+    "anesrake",  # Raking.
+    "arrayhelpers",  # Convenience functions for arrays.
+    "bayesplot",  # Plotting functions for posterior analysis.
+    "bbc/bbplot",  # BBC styles for ggplot2
+    "bridgesampling",  # Bridge sampling for Stan models.
+    "car",  # For car::Anova().
+    "diagram",  # Visualize simple graphs/networks.
+    "data.table",  # [Update this.]
+    "directlabels",  # Used by our PubMedTrends.R.
+    "doParallel",  # Parallel computing.
+    "doSNOW",  # Use SNOW for parallel computing.
+    "dplyr",  # [Update this.]
+    # "easypackages",  # Easier installation of other packages. PREFER: pacman, pak.
+    "Epi",  # epidemiology functions inc. the "poisreg" Poisson regression family. \
+    "extrafont",  # fonts
+    "ez",  # ezANOVA; simple analysis of variance.
+    "flextable",  # Pretty table creation: https://ardata-fr.github.io/flextable-book/index.html
+    "ftExtra",  # For markup (e.g. superscript) within flextable.
+    "gdata",  # Miscellaneous data manipulation tools.
+    "ggforce",  # e.g. better faceting \
+    "ggmcmc",  # Tools for analysing MCMC simulations from Bayesian inference.
+    "ggtext",  # provides element_markdown() etc.
+    "gplots",  # Misc. plotting functions. \
+    "gridtext",  # for textbox_grob()
+    "gtools",  # Programming assistance.
+    "HDInterval",  # Highest density intervals (Kruschke et al.)
+    "Hmisc",  # Harrell Miscellaneous.
+    "imager",  # Image processing.
+    "invgamma",  # inverse gamma distribution
+    "languageR",  # Analysing linguistic data (NLP).
+    "LMERConvenienceFunctions",  # Help with lmer().
+    "lmerTest",  # Provides p values for lme4::lmer().
+    "loo",  # Leave-one-out cross-validation and WAIC for Bayesian models.
+    "lsmeans",  # Least-squares means.
+    "ltm",  # Latent trait models. \
+    "matrixStats",  # High-performance matrix functions.
+    "MCMCglmm",  # MCMC generalized linear mixed models.
+    "moments",  # E.g. skewness, kurtosis.
+    "multcomp",  # Multiple comparisons for generalized linear models etc.
+    "multidplyr",  # Parallel processing for dplyr (part of tidyverse).
+    "MuMIn",  # Multi-model inference.
+    "NightingaleHealth/ggforestplot",  # forest plots in ggplot
+    "nortest",  # Tests for normality.
+    "officer",  # Interface with Office (Word, Powerpoint) documents; https://davidgohel.github.io/officer/
+    "openxlsx",  # Manipulate Excel files.
+    "optimParallel",  # Parallel version of L-BFGS-B method for optim().
+    "pacman",  # R package management.
+    "patchwork",  # Arrange ggplot plots; https://patchwork.data-imaginist.com/.
+    "pda",  # Privacy-preserving distributed algorithms.
+    "plotrix",  # Plotting functions.
+    "popbio",  # Matrix population models.
+    "popEpi",  # e.g. splitMulti().
+    "progress",  # progress bar: seems to be included anyway
+    "proto",  # Prototype object-based programming.
+    "pwr",  # Power calculations.
+    "raster",  # Geographic data analysis and modelling.
+    "rcompanion",  # e.g. wilcoxonZ
+    "readODS",  # Read OpenOffice files.
+    "readxl",  # fast XLSX read
+    "RMySQL",  # Direct connection to MySQL databases.
+    "rstan",  # Stan (Bayesian statistical inference; https://mc-stan.org/)
+    "rstanarm",  # Bayesian applied regression modeling via Stan.
+    "rstantools",  # Tools for packages interfacing with Stan.
+    "semver",  # Semantic version numbering.
+    "sf",  # Simple spatial features.
+    "shinystan",  # Visualisation for Stan models.
+    "snow",  # Simple Network of Workstations (parallel computing).
+    "sp",  # Spatial package, for maps.
+    "sqldf",  # Manipulate data frames using SQL.
+    "survminer",  # Extras for survival analysis/visualization. \
+    "svglite",  # SVG export
+    "synthpop",  # Produce synthetic data resembling confidential data.
+    "terra",  # Spatial data analysis.
+    "TTR",  # Technical Trading Rules.
+    "writexl",  # fast XLSX write
+    "visreg",  # Visualize regression models.
+    "XLConnect",  # Read/write/manipulate Excel files.
+    "xlsx"  # Read/write/manipulate Excel files.
+))
+
+
+# =============================================================================
+# Try harder for ones that don't want to go to the version we want
+# =============================================================================
+
+cat("- R: Installing packages, stage 2\n")
+
+# pak::cache_clean()  # seems to make no difference
+
+# pak::pak("data.table@1.18.6.1")
+# pak::pak("dplyr@1.2.1")
+#
+# The curious thing is that this doesn't achieve the desired version in the
+# script; for data.table, it stays at or gets to 1.15.4. Interactively, though,
+# it offers 1.18.6.1 but then fails with:
+#    "is not a valid R package, it is an empty archive"
+# Possibly this bug: https://github.com/r-lib/pak/issues/658
+
+pak::pak("data.table", upgrade = TRUE)
+pak::pak("dplyr", upgrade = TRUE)
+
+
+# =============================================================================
+# Cleanup
+# =============================================================================
+
+cat("- R: Package cache cleanup\n")
+
+pak::cache_clean()
+
+
+# =============================================================================
+# Not available for current version of R (?)
+# =============================================================================
+#
+# NOT AVAILABLE FOR CURRENT VERSION OF R (e.g. for some: 3.6.3, 4.1.2, or
+# 4.3.1):
+#
+#   Kmisc
+#       Including cat.cb, to write to the clipboard.
+#   maptools
+#       For spatial maps.
+#       https://cran.r-project.org/web/packages/maptools/index.html
+#       Removed; suggests 'sf' or 'terra' instead.
+#   rgdal
+#       Geospatial.
+#       https://cran.r-project.org/web/packages/rgdal/index.html
+#       Removed; suggests 'sf' or 'terra' instead.
+#   rgeos
+#       Interface to GEOS (Geometry Engine - Open Source).
+#       https://cran.r-project.org/web/packages/rgeos/index.html
+#       Removed; suggests 'sf' or 'terra' instead.
+#   xtermStyle
+#       Terminal text formatting.
+#       https://cran.r-project.org/web/packages/xtermStyle/index.html
+#       Removed.
+
+
+# =============================================================================
+# Don't need:
+# =============================================================================
+#
+#   RODBC
+#       ODBC connector; use DBI or odbc instead;
+#       https://www.mainard.co.uk/post/database-connections-in-r/
--- notes/R_library_notes.R
+++ notes/R_library_notes.R
@@ -1,4 +1,68 @@
 # =============================================================================
+# Package management systems
+# =============================================================================
+#
+# -----------------------------------------------------------------------------
+# base
+# -----------------------------------------------------------------------------
+#
+# Useful for checking in user code, e.g.
+#
+#   stopifnot(packageVersion("data.table") >= package_version("1.16.0"))
+#
+# -----------------------------------------------------------------------------
+# pacman
+# -----------------------------------------------------------------------------
+# - https://trinker.github.io/pacman_dev/
+#
+# For version control -- pacman 0.5.1 is not reliable.
+#
+#   pacman::p_install_version('<package>', version = '<version>')
+#
+# ... sometimes just flat-out installs the wrong version. Avoid.
+#
+# -----------------------------------------------------------------------------
+# devtools
+# -----------------------------------------------------------------------------
+# - https://devtools.r-lib.org/
+#
+#   devtools::install_version(
+#       'PACKAGE',
+#       version = 'VERSION',
+#       repos = c('SPECIFIC_URL_IF_REQUIRED', getOption('repos')),
+#       dependencies = TRUE
+#   )"
+#
+# But they have deprecated themselves in favour of pak:
+#
+#   https://devtools.r-lib.org/reference/install-deprecated.html
+#
+# -----------------------------------------------------------------------------
+# packrat
+# -----------------------------------------------------------------------------
+# - https://rstudio.github.io/packrat/
+#
+# -----------------------------------------------------------------------------
+# pak
+# -----------------------------------------------------------------------------
+# - https://pak.r-lib.org/
+#
+# Defaults towards the latest versions. But you can be specific:
+#
+#   pak::pak("PACKAGE")
+#   pak::pak("PACKAGE@VERSION")
+#   pak::pak("PACKAGE@>=VERSION")
+#       # though not really: "! Version ranges are not implemented yet."
+#
+# GitHub installation: e.g.
+#
+#   pak::pak("tidyverse/tibble")
+#
+# See:
+#   ?"Package sources"
+
+
+# =============================================================================
 # dplyr (part of tidyverse)
 # =============================================================================
 #
@@ -18,8 +82,11 @@
 #   Older versions: TRUE ~ ...
 #   Newer versions: .default = ...
 #       ... "TRUE ~" still works, but is deprecated.
-#
-#
+
+library(dplyr)
+stopifnot(packageVersion("dplyr") >= package_version("1.2.0"))
+
+
 # =============================================================================
 # data.table
 # =============================================================================
@@ -33,5 +100,43 @@
 # -----------------------------------------------------------------------------
 # Progress bars
 # -----------------------------------------------------------------------------
+#
+# - Manual methods: https://github.com/Rdatatable/data.table/issues/1409
+#
+# - Automatic method: available from version 1.16.0 (25 Aug 2024).
+#   See https://cran.r-project.org/web/packages/data.table/news/news.html.
+#   Availability is indicated by the "showProgress" option in ?data.table,
+#   which defaults to:
+#       showProgress = getOption("datatable.showProgress", interactive())]
 
-# ***
+library(data.table)
+stopifnot(packageVersion("data.table") >= package_version("1.16.0"))
+
+test_data_table_progress_bar <- function(simple_by = TRUE, with_assign = TRUE)
+{
+    # Tested with v1.18.6.1, 12 Sep 2006.
+
+    n <- 10
+    d <- data.table(grouper = LETTERS[1:n], value = 1:n)
+
+    groupfn1 <- function(BY, SD, sleep = 1) {
+        # cat("- Sleeping for", sleep, "\n")
+        Sys.sleep(sleep)
+        return(list(p = 1, q = 1))
+    }
+
+    if (simple_by) {
+        cat("- Testing data.table progress bar: (1) Standard 'by'\n")
+        g = d[, groupfn1(.BY, .SD), by = grouper]
+        print(g)
+        cat("  ... outcome: DISPLAYS progress bar.\n")
+    }
+
+    if (with_assign) {
+        cat("- Testing data.table progress bar: (1) Direct ':='\n")
+        dc <- copy(d)
+        dc[, c("p", "q") := groupfn1(.BY, .SD), by = grouper]
+        print(dc)
+        cat("  ... outcome: DOES NOT DISPLAY progress bar.\n")
+    }
+}
